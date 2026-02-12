af37ca5ea11713f92c2fff5bf4d93fab23b05d54
Тимофей Романович <timofei.web@gmail.com>
2026-02-12 16:38:55 +0000

    error -> page-screen

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import PageScreen from "@/components/PageScreen/PageScreen";
+import Btn from "@/components/Btn/Btn";
+import { ArrowLeft } from "@/components/icons";
 
 export default function NotFound() {
   return (
     <main>
-      <PageScreen />
+      <PageScreen>
+        <Image src="/images/hero-image.png" alt="rapha-polyclinique" className="page-screen__bg" width={1920} height={1070} />
+        <h1 className="simple-title simple-title--large">Error 404</h1>
+        <Btn text="Back to home" icon={ArrowLeft} iconPosition="left" className="btn--blue-l page-screen__btn" />
+      </PageScreen>
     </main>
   );
 }
--- src/components/PageScreen/PageScreen.jsx
+++ src/components/PageScreen/PageScreen.jsx
@@ -1,15 +1,10 @@
-import Btn from "@/components/Btn/Btn";
-import Image from "next/image";
-import { ArrowLeft } from "@/components/icons";
 import "./PageScreen.scss";
 
-export default function PageScreen() {
+export default function PageScreen({ children }) {
     return (
         <section className="page-screen">
             <div className="center-wrap">
-            <Image src="/images/hero-image.png" alt="rapha-polyclinique" className="page-screen__bg" width={1920} height={1070} />
-                <h1 className='simple-title simple-title--large'>Error 404</h1>
-                <Btn text="Back to home" icon={ArrowLeft} iconPosition="left" className='btn--blue-l page-screen__btn' />
+                {children}
             </div>
         </section>
     );
