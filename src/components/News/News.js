f6dedb4bd2cc0ebeb69799e7a71ed22a74fbbbbc
Тимофей Романович <timofei.web@gmail.com>
2026-02-18 23:03:26 +0000

    implement home page jsx

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,39 +1,30 @@
 import "./News.scss";
 import Image from "next/image";
-import Link from 'next/link'
-import Btn from "@/components/ui/Btn/Btn";
+import Link from 'next/link';
 
+const DEFAULT_TITLE = "News";
+const DEFAULT_ITEMS = [
+    { image: { src: "/images/news1.png", alt: "news-image" }, title: "Lorem ipsum dolor", text: "Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, " },
+    { image: { src: "/images/news2.png", alt: "news-image" }, title: "Lorem ipsum dolor", text: "Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, " },
+    { image: { src: "/images/news3.png", alt: "news-image" }, title: "Lorem ipsum dolor", text: "Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, " },
+];
 
-export default function news() {
+export default function News({ title = DEFAULT_TITLE, items = DEFAULT_ITEMS }) {
     return (
         <section className="news">
             <div className="center-wrap center-wrap--small">
-            <h2 className="simple-title simple-title--center news__title">News</h2>
+            <h2 className="simple-title simple-title--center news__title">{title}</h2>
             <div className="news__list">
-                <div className="news__item">
-                    <Image src="/images/news1.png" alt="news-image" className="news__item-img" width={500} height={400} />
-                    <h3 className="news__item-title">Lorem ipsum dolor</h3>
-                    <div className="news__item-text content">
-                        <p>Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, </p>
+                {items.map((item, i) => (
+                    <div key={i} className="news__item">
+                        <Image src={item.image.src} alt={item.image.alt} className={i === 2 ? "news__item-img white-header" : "news__item-img"} width={500} height={400} />
+                        <h3 className="news__item-title">{item.title}</h3>
+                        <div className="news__item-text content">
+                            <p>{item.text}</p>
+                        </div>
+                        <Link href="#" className='news__item-link link link-hover'>Read more</Link>
                     </div>
-                    <Link href="#" className='news__item-link link link-hover'>Read more</Link>
-                </div>
-                <div className="news__item">
-                    <Image src="/images/news2.png" alt="news-image" className="news__item-img" width={500} height={400} />
-                    <h3 className="news__item-title">Lorem ipsum dolor</h3>
-                    <div className="news__item-text content">
-                        <p>Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, </p>
-                    </div>
-                    <Link href="#" className='news__item-link link link-hover'>Read more</Link>
-                </div>
-                <div className="news__item">
-                    <Image src="/images/news3.png" alt="news-image" className="news__item-img white-header" width={500} height={400} />
-                    <h3 className="news__item-title">Lorem ipsum dolor</h3>
-                    <div className="news__item-text content">
-                        <p>Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, </p>
-                    </div>
-                    <Link href="#" className='news__item-link link link-hover'>Read more</Link>
-                </div>
+                ))}
             </div>
             </div>
         </section>    
