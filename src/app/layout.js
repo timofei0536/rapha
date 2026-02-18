51116c918c56b52b5fb05fa735a96b8766ca0359
Тимофей Романович <timofei.web@gmail.com>
2026-02-18 22:42:59 +0000

    border-rradius fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -94,7 +94,6 @@ export default function RootLayout({ children }) {
 // 1) chart уточнить
 // 2) careers form.
 // 3) news - pagination.
-// 4) image border-radius
 // 5) services -> ask about it.
 
 // 1) html to loops
