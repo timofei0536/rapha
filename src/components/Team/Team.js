12ccfff7f469e1e1c6a21e5689bdb7356f177f1f
Тимофей Романович <timofei.web@gmail.com>
2026-02-20 22:44:08 +0000

    implement home page

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -11,37 +11,33 @@ export default function Team(props) {
                         <div className="team__left">
                             <h2 className="team__small-title simple-title">{subtitle}</h2>
                             <span className="team__title">{title}</span>
-                            <div className="team__text content" dangerouslySetInnerHTML={{ __html: content ?? "" }} />
+                            <div className="team__text content" dangerouslySetInnerHTML={{ __html: content }} />
                             <Btn text='Read More' className="btn--orange" />
                         </div>
                         <div className="team__gallery">
                             <div className="team__gallery-part team__gallery-part--left">
-                                {(gallery?.left ?? []).map((item, i) =>
-                                    item?.image?.src ? (
-                                        <Image
-                                            key={i}
-                                            src={item.image.src}
-                                            alt={item.image.alt ?? ""}
-                                            className="team__gallery-img"
-                                            width={item.width ?? 434}
-                                            height={item.height ?? 412}
-                                        />
-                                    ) : null
-                                )}
+                                {(gallery?.left ?? []).map((item, i) => (
+                                    <Image
+                                        key={i}
+                                        src={item.image.src}
+                                        alt={item.image.alt}
+                                        className="team__gallery-img"
+                                        width={item.width ?? 434}
+                                        height={item.height ?? 412}
+                                    />
+                                ))}
                             </div>
                             <div className="team__gallery-part team__gallery-part--right">
-                                {(gallery?.right ?? []).map((item, i) =>
-                                    item?.image?.src ? (
-                                        <Image
-                                            key={i}
-                                            src={item.image.src}
-                                            alt={item.image.alt ?? ""}
-                                            className="team__gallery-img"
-                                            width={item.width ?? 434}
-                                            height={item.height ?? 224}
-                                        />
-                                    ) : null
-                                )}
+                                {(gallery?.right ?? []).map((item, i) => (
+                                    <Image
+                                        key={i}
+                                        src={item.image.src}
+                                        alt={item.image.alt}
+                                        className="team__gallery-img"
+                                        width={item.width ?? 434}
+                                        height={item.height ?? 224}
+                                    />
+                                ))}
                             </div>
                         </div>
                 </div>
