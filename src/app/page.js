7f5e3c2953013992a5e9709482eaa6c1cbf5549d
Тимофей Романович <timofei.web@gmail.com>
2026-02-20 17:10:19 +0000

    implement infra

--- src/app/page.js
+++ src/app/page.js
@@ -3,20 +3,30 @@ import Services from "../components/Services/Services";
 import Team from "../components/Team/Team";
 import Infra from "../components/Infra/Infra";
 import News from "../components/News/News";
+import { getPagePropsFromSearchParams } from "@/lib/wp-api";
 import { HeroDefaults } from "@/components/Hero/defaults";
 import { ServicesDefaults } from "@/components/Services/defaults";
 import { TeamDefaults } from "@/components/Team/defaults";
 import { InfraDefaults } from "@/components/Infra/defaults";
 import { NewsDefaults } from "@/components/News/defaults";
 
-export default function Home() {
+const HOME_PAGE_SLUG = "page";
+
+export default async function Home({ searchParams }) {
+  const infraProps = await getPagePropsFromSearchParams(
+    HOME_PAGE_SLUG,
+    "infra",
+    InfraDefaults,
+    searchParams
+  );
+
   return (
-      <main>
-         <Hero {...HeroDefaults} />
-         <Services {...ServicesDefaults} />
-         <Infra {...InfraDefaults} />
-         <Team {...TeamDefaults} />
-         <News {...NewsDefaults} />
-      </main>
+    <main>
+      <Hero {...HeroDefaults} />
+      <Services {...ServicesDefaults} />
+      <Infra {...infraProps} />
+      <Team {...TeamDefaults} />
+      <News {...NewsDefaults} />
+    </main>
   );
 }
