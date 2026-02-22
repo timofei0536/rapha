ff7e38d51c8f292a0dce3439865c6bd60b7b3f34
Тимофей Романович <timofei.web@gmail.com>
2026-02-22 21:24:14 +0000

    fix pin

--- src/animations/sectionPin.js
+++ src/animations/sectionPin.js
@@ -3,11 +3,6 @@ const PINS = [
   { section: '.new__wrap', pin: '.new__img' },
 ];
 
-function getEnd(sectionEl) {
-  const pb = parseFloat(getComputedStyle(sectionEl).paddingBottom) || 0;
-  return `bottom bottom+=${pb + 40}`;
-}
-
 export function sectionPin() {
   const headerEl = document.querySelector('.header');
   const start = `top ${(headerEl ? headerEl.offsetHeight : 0) + 50}px`;
@@ -20,13 +15,10 @@ export function sectionPin() {
     window.ScrollTrigger.create({
       trigger: sectionEl,
       endTrigger: sectionEl,
-      // start,
-      start: "top top",
-      // end: getEnd(sectionEl),
-      end: "+="+ ( sectionEl.offsetHeight - pinEl.offsetHeight * 1 ) + "px",
+      start,
+      end: '+=' + (sectionEl.offsetHeight - pinEl.offsetHeight) + 'px',
       pin: pinEl,
       invalidateOnRefresh: true,
     });
-
   });
 }
