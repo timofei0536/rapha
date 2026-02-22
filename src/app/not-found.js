fdf3622dba42506a174b11dae89b25c0fcb1f382
Тимофей Романович <timofei.web@gmail.com>
2026-02-22 21:40:54 +0000

    add link

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -9,7 +9,7 @@ export default function NotFound() {
       <PageScreen className="page-screen--error">
         <Image src="/images/404.png" alt="rapha-polyclinique" className="page-screen__bg" width={1920} height={1070} />
         <h1 className="simple-title simple-title--large">Error 404</h1>
-        <Btn text="Back to home" icon={ArrowLeft} iconPosition="left" className="btn--blue-l page-screen__btn" />
+        <Btn text="Back to home" icon={ArrowLeft} iconPosition="left" className="btn--blue-l page-screen__btn" href="/" />
       </PageScreen>
     </main>
   );
